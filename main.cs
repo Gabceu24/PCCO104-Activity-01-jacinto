f7b209cf0ca6e7147029cc1b596adb4199b50b7e
jacinto2130843 <31803624-jacinto2130843@users.noreply.replit.com>
2024-02-03 02:52:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Gab";
+    string lastName = "jacinto";
+    int age = 17; 
+    Console.WriteLine ($"Goodbye World {firstName} {lastName}");
+    Console.WriteLine ("Goodbye World" + FirstName + "" + lastName + "" + age);
+  }
+}
